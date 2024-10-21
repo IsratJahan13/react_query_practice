3c5f7fb3a5a55a581b18aeac44e7ff235f13e803
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-10-21 16:49:00 +0300

    added usestate for loading page

--- src/components/Superheroes.page.jsx
+++ src/components/Superheroes.page.jsx
@@ -3,12 +3,18 @@ import React, { useEffect, useState } from 'react';
 
 const SuperheroesPage = () => {
     const [data, setData] = useState([])
+    const [isloading, setIsLoading] = useState(true)
 
     useEffect(() => {
         axios.get('http://localhost:4000/superheroes').then((response) => {
             setData(response.data)
+            setIsLoading(false)
         })
     }, [])
+
+    if(isloading) {
+        return <h2>Loading...</h2>
+    }
 
     return (
         <div>
